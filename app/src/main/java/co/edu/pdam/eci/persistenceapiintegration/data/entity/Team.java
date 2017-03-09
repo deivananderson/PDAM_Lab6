e10bec10caeb80e95d2bb648f9b8254033824087
deivananderson <deyvananderson@hotmail.com>
2017-03-09 16:14:38 -0500

    Creacion repositorio

---
+++ app/src/main/java/co/edu/pdam/eci/persistenceapiintegration/data/entity/Team.java
@@ -0,0 +1,11 @@
+package co.edu.pdam.eci.persistenceapiintegration.data.entity;
+
+/**
+ * @author Santiago Carrillo
+ */
+
+//TODO add database annotations and proper model structure
+public class Team
+    extends BaseEntity
+{
+}
---
+++ app/src/main/java/co/edu/pdam/eci/persistenceapiintegration/ui/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package co.edu.pdam.eci.persistenceapiintegration.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.edu.pdam.eci.persistenceapiintegration.R;
+
+public class MainActivity
+    extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
